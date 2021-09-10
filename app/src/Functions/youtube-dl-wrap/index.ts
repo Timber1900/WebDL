ef87082b739401f2437ac521a99894cbf8890397
Timber1900 <timber.gamer@gmail.com>
2021-09-10 18:18:30 +0100

    remove nwjs files

--- app/src/Functions/youtube-dl-wrap/index.ts
+++
@@ -1,5 +0,0 @@
-import YoutubeDlWrap from 'youtube-dl-wrap';
-import { join } from 'path';
-import { downloadPath } from '../../Constants';
-
-export const youtubeDlWrap = new YoutubeDlWrap(join(downloadPath, process.platform === 'win32' ? 'youtube-dl.exe' : 'youtube-dl'));
