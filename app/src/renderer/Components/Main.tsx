e50c46609cfe9f9e4bb82e594679453864f36c42
Timber1900 <timber.gamer@gmail.com>
2021-09-12 21:36:39 +0100

    Started work on captions

--- app/src/renderer/Components/Main.tsx
+++ app/src/renderer/Components/Main.tsx
@@ -9,9 +9,8 @@ const Main = () => {
   return(
     <div className="flex flex-col items-center justify-start overflow-y-scroll bg-gray-200 dark:bg-gray-700">
       {curQueue.map(({ clips, curQual, download, duration, ext, id, info, merge, quality, show, thumbnail, title}, i) => (
-        <Item quality={quality} key={i} clips={clips} curQual={curQual} download={download} duration={duration} ext={ext} i={i} id={id} info={info} merge={merge} show={show} thumbnail={thumbnail} title={title} />
+        <Item  quality={quality} key={i} clips={clips} curQual={curQual} download={download} duration={duration} ext={ext} i={i} id={id} info={info} merge={merge} show={show} thumbnail={thumbnail} title={title} />
       ))}
-
     </div>
   );
 };
